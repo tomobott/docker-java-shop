e4eb09863a3adb306cf15ec4769a0ad1c1f85f04
Tom O'Brien <tobrien@cbnco.com>
2019-06-04 14:32:56 -0400

    update to use my dockerhub repo

--- build_all_and_publish_dockerhub.sh
+++ build_all_and_publish_dockerhub.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+# $1 is the version tag for the image
+[ -z $1 ] && echo "you must supply a version tag for the image" && exit 1
 
 cd shopfront
 mvn clean install
-if docker build -t apetrovya/djshopfront . ; then
-  docker push apetrovya/djshopfront
+if docker build -t tomobott/djshopfront:$1 . ; then
+  docker push tomobott/djshopfront:$1
 fi
 cd ..
 
 cd productcatalogue
 mvn clean install
-if docker build -t apetrovya/djproductcatalogue . ; then
-  docker push apetrovya/djproductcatalogue
+if docker build -t tomobott/djproductcatalogue:$1 . ; then
+  docker push tomobott/djproductcatalogue:$1
 fi
 cd ..
 
 cd stockmanager
 mvn clean install
-if docker build -t apetrovya/djstockmanager . ; then
-  docker push apetrovya/djstockmanager
+if docker build -t tomobott/djstockmanager:$1 . ; then
+  docker push tomobott/djstockmanager:$1
 fi
 cd ..
